d792bd6045654f908ad4a51add61e4b36340b6b9
Zahniar Adirahman <zahniar88@gmail.com>
2021-06-03 19:20:30 +0700

    make new query

--- models/Entity.php
+++ models/Entity.php
@@ -1,21 +1,8 @@
 <?php
 defined("BASEPATH") OR die("No direct access allowed");
 
-require_once __DIR__ . "/../core/Database/InsertQuery.php";
-require_once __DIR__ . "/../core/Database/SelectQuery.php";
-require_once __DIR__ . "/../core/Database/MergeWithQuery.php";
-require_once __DIR__ . "/../core/Database/JoinQuery.php";
-require_once __DIR__ . "/../core/Database/UpdateQuery.php";
-require_once __DIR__ . "/../core/Database/DeleteQuery.php";
 
-class Entity extends CI_Model {
-
-    use InsertQuery, 
-        SelectQuery, 
-        MergeWithQuery, 
-        JoinQuery, 
-        UpdateQuery,
-        DeleteQuery;
+class Entity extends CI_Model {    
 
     /**
      * define default table name
